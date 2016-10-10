bd24bf9ae4a0c7b4e96009717397f69e04ca8925
Luan Andrade <luansandrade92@hotmail.com>
2016-10-09 21:46:51 -0300

    empregos line graph

--- 02 Intro Analise/02 np e pandas/empregos15.py
+++ 02 Intro Analise/02 np e pandas/empregos15.py
@@ -8,25 +8,14 @@ employments = pd.read_csv('employment_above_15.csv')
 #print(employments['Brazil'])
 
 employments = employments.set_index('Country')
-Brazil = employments.loc('Brazil')
-
-#for employment in employments:
-#	if employment['Country'] == 'Brazil':
-#		Brazil = employment
-#		del Brazil['Country']
-
+Brazil = employments.loc['Brazil']
 print(Brazil)
-x = []
-y = []
-for key,value in Brazil.items():
-	x.append(int(key))
-	y.append(float(value))
 
-print(y)
-plt.xlabel("Anos")
-plt.ylabel("Empregos")
-plt.title("Empregos +15 anos - Brasil")
+#print(y)
+#plt.xlabel("Anos")
+#plt.ylabel("Empregos")
+#plt.title("Empregos +15 anos - Brasil")
 
 #plt.plot(x,y)
-plt.hist(y)
+plt.plot(Brazil)
 plt.show()
