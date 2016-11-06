279d7f1a0312f8c8715aa7f1c846331623c69708
Luan Andrade <luansandrade92@hotmail.com>
2016-11-06 17:14:52 -0200

    Quiz: consertando o espaço * erro no [20]

---
+++ 03 Limpeza de Dados/06-Exercicios_praticos3/area.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+In this problem set you work with cities infobox data, audit it, come up with a
+cleaning idea and then clean it up.
+
+Since in the previous quiz you made a decision on which value to keep for the
+"areaLand" field, you now know what has to be done.
+
+Finish the function fix_area(). It will receive a string as an input, and it
+has to return a float representing the value of the area or None.
+You have to change the function fix_area. You can use extra functions if you
+like, but changes to process_file will not be taken into account.
+The rest of the code is just an example on how this function can be used.
+"""
+import codecs
+import csv
+import json
+import pprint
+
+CITIES = 'cities.csv'
+
+
+def fix_area(area):
+
+    # YOUR CODE HERE
+    if area == "NULL" or area == "":
+        return None
+    if area[0] == "{":
+        areas = area.replace("{","").replace("}","").split("|")
+        
+        if areas[0].count('0') > areas[1].count('0'):
+            area = areas[1]
+        else:
+            area = areas[0]
+
+    return float(area)
+
+
+
+def process_file(filename):
+    # CHANGES TO THIS FUNCTION WILL BE IGNORED WHEN YOU SUBMIT THE EXERCISE
+    data = []
+
+    with open(filename, "r") as f:
+        reader = csv.DictReader(f)
+
+        #skipping the extra metadata
+        for i in range(3):
+            next(reader)
+
+        # processing file
+        for line in reader:
+            # calling your function to fix the area value
+            if "areaLand" in line:
+                line["areaLand"] = fix_area(line["areaLand"])
+            data.append(line)
+
+    return data
+
+
+def test():
+    data = process_file(CITIES)
+
+    print ("Printing three example results:")
+    for n in range(3,8):
+        pprint.pprint(data[n]["areaLand"])
+
+    pprint.pprint(data[20]["areaLand"])
+        
+
+    assert data[3]["areaLand"] == None        
+    assert data[8]["areaLand"] == 55166700.0
+    assert data[20]["areaLand"] == 14581600.0
+    assert data[33]["areaLand"] == 20564500.0    
+
+
+if __name__ == "__main__":
+    test()
