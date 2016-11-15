bcc6a9dab5548f2ba7dffbc77f0f3a49301db4d8
Luan Andrade <luansandrade92@hotmail.com>
2016-11-15 00:21:28 -0200

    group part 2

--- 03 Limpeza de Dados/07-Mongodb/group.py
+++ 03 Limpeza de Dados/07-Mongodb/group.py
@@ -32,16 +32,18 @@ def get_db(db_name):
 
 def make_pipeline():
     # complete the aggregation pipeline
-    pipeline = []
+    pipeline = [{"$group":{"_id":"$source",
+                            "count": {"$sum":1} } } ,
+                {"$sort":{"count":-1} }]
     return pipeline
 
 def tweet_sources(db, pipeline):
     return [doc for doc in db.tweets.aggregate(pipeline)]
 
 if __name__ == '__main__':
-    db = get_db('twitter')
+    db = get_db('impeachment')
     pipeline = make_pipeline()
     result = tweet_sources(db, pipeline)
     import pprint
-    pprint.pprint(result[0])
+    pprint.pprint(result)
     assert result[0] == {u'count': 868, u'_id': u'web'}
